96532d165bf31736b3a41da2c8c6ceda1120c519
Moh4meDEV <168861457+moh4meDev@users.noreply.github.com>
2024-08-31 16:30:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
